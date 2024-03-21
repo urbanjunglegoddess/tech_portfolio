b46fc59ab50093ae73da6db746ba7f63aef0810d
Omegea Hunter <omegeadhunter@gmail.com>
2024-03-21 12:13:26 -0400

    Did a youtube javascrpit crash course to create a html cheat sheet

--- tutorials/javascript/js-crash-course/main.js
+++ tutorials/javascript/js-crash-course/main.js
@@ -12,6 +12,8 @@ age = 31;
 
 console.log(age);
 
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 //Data Types
 
 // String, Numbers, Boolean, null, undefined, symbol
@@ -29,6 +31,7 @@ console.log(typeof x);
 console.log(typeof y);
 console.log(typeof z);
 
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // Strings
 // Concatenation
@@ -43,6 +46,8 @@ console.log(hello.toUpperCase());
 console.log(hello.toLowerCase());
 console.log(hello.substring(0, 5).toUpperCase());
 console.log(hello.split(''));
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // Arrays - variables that hold multiple values
 const numbers = new Array(1,2,3,4,5);
@@ -62,6 +67,8 @@ fruits.pop();
 console.log(fruits);
 console.log(Array.isArray(fruits));
 console.log(fruits.indexOf('oranges'));
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // Object Literals
 const person = {
@@ -89,6 +96,8 @@ console.log(city);
 person.email = 'john@gmail.com';
 console.log(person);
 
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 // Arrays of Objects
 const todos = [
     {
@@ -111,10 +120,13 @@ const todos = [
 console.log(todos);
 console.log(todos[1].text);
 
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // JSON
 const todoJSON = JSON.stringify(todos);
 console.log(todoJSON);
 
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // For
 for(let i = 0; i < 10; i++) {
@@ -132,11 +144,11 @@ while(i < 10) {
 for(let i = 0; i < todos.length; i++) {
     console.log(todos[i].text);
 }
-
+// for of
 for(let todo of todos) {
     console.log(todo.id);
 }
-
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // High Order Array Methods
 // forEach, map, filter
 // forEach
@@ -162,14 +174,17 @@ const todoCompletedText = todos.filter(function(todo) {
     return todo.text;}
 );
 console.log(todoCompletedText);
-
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Conditionals
 // if, else, else if, ternary operator, switch
+// if
 const a = 10;
 if(a === 10) {
     console.log('x is 10');
 }
 
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// else
 const b = 20;
 if(b === 10) {
     console.log('y is 10');
@@ -178,6 +193,9 @@ else {
     console.log('y is NOT 10');
 }
 
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// else if
 const c = 30;
 if(c === 10) {
     console.log('z is 10');
@@ -189,10 +207,14 @@ else {
     console.log('z is less than 10');
 }
 
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 // Ternary Operator
 const d = 10;
 const color = d > 10 ? 'red' : 'blue';
 console.log(color);
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // Switch
 switch(color) {
@@ -207,15 +229,22 @@ switch(color) {
         break;
 }
 
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 // Functions
 function addNums(num1, num2) {
     return num1 + num2;
 }
 console.log(addNums(5, 4));
 
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
 // Arrow Function
-const addNums = (num1, num2) => num1 + num2;
-console.log(addNums(5, 4));
+const addNums2 = (num1, num2) => num1 + num2;
+console.log(addNums2(5, 4));
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // Object-Oriented Programming
 // Constructor Function
@@ -236,9 +265,10 @@ const person1 = new Person('John', 'Doe', '4-3-1980');
 const person2 = new Person('Mary', 'Smith', '3-6-1970');
 console.log(person1);
 console.log(person2.dob);
-console.log(person1.getBirthYear());
-console.log(person1.getFullName());
+console.log(person1.getBirthYear);
+console.log(person1.getFullName);
 
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // Prototypes
 function Person(firstName, lastName, dob) {
@@ -254,16 +284,17 @@ Person.prototype.getFullName = function() {
 }
 
 // Instantiate Object
-const person1 = new Person('John', 'Doe', '4-3-1980');
-const person2 = new Person('Mary', 'Smith', '3-6-1970');
-console.log(person1);
-console.log(person2.dob);
-console.log(person1.getBirthYear());
-console.log(person1.getFullName());
+const person3 = new Person('John', 'Doe', '4-3-1980');
+const person4 = new Person('Mary', 'Smith', '3-6-1970');
+console.log(person3);
+console.log(person4.dob);
+console.log(person3.getBirthYear);
+console.log(person4.getFullName);
 
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // ES6 Classes
-class Person {
+class Persons {
     constructor(firstName, lastName, dob) {
         this.firstName = firstName;
         this.lastName = lastName;
@@ -278,21 +309,38 @@ class Person {
 }
 
 // Instantiate Object
-const person1 = new Person('John', 'Doe', '4-3-1980');
-const person2 = new Person('Mary', 'Smith', '3-6-1970');
-console.log(person1);
-console.log(person2.dob);
-console.log(person1.getBirthYear());
+const person5 = new Persons('John', 'Doe', '4-3-1980');
+const person6 = new Persons('Mary', 'Smith', '3-6-1970');
+console.log(person5);
+console.log(person6.dob);
+console.log(person5.getBirthYear());
 
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // DOM
 // Single Element Selectors
 // document.getElementById()
-console.log(document.getElementById('my-form'));
 const form = document.getElementById('my-form');
 console.log(form);
+console.log(document.getElementById('my-form'));
 console.log(document.querySelector('h1'));
 
+myForm.addEventListener('submit', onSubmit() {
+    function onSubmit(e) {
+        e.preventDefault();
+
+        if(nameInput.value === '' || emailInput.value === '') {
+            msg.classList.add('error');
+            msg.innerHTML = 'Please enter all fields';
+            setTimeout(() => msg.remove(), 3000);
+        }   else {
+            const li = document.createElement('li');
+            li.appendChild(document.createTextNode(`${nameInput.value}: ${emailInput.value}`));
+            userList.appendChild(li);
+            nameInput.value = '';
+            emailInput.value = '';
+    }
+}
 // Multiple Element Selectors
 // document.querySelectorAll()
 console.log(document.querySelectorAll('.item'));
@@ -307,7 +355,7 @@ const ul = document.querySelector('.items');
 // ul.remove();
 // ul.lastElementChild.remove();
 ul.firstElementChild.textContent = 'Hello';
-ul.children[1].innerText = 'Brad';
+ul.children[1].innerconsole.log(document.getElementById('my-form'));Text = 'Brad';
 ul.lastElementChild.innerHTML = '<h1>Hello</h1>';
 
 const btn = document.querySelector('.btn');
@@ -331,7 +379,7 @@ nameInput.addEventListener('input', (e) => {
 
 // Form Event
 const myForm = document.querySelector('#my-form');
-const nameInput = document.querySelector('#name');
+const nameInput2 = document.querySelector('#name');
 const emailInput = document.querySelector('#email');
 const msg = document.querySelector('.msg');
 const userList = document.querySelector('#users');
@@ -353,12 +401,12 @@ sessionStorage.setItem('name', 'Beth');
 localStorage.removeItem('name');
 
 // get from storage
-const name = localStorage.getItem('name');
-const age = localStorage.getItem('age');
+const name1 = localStorage.getItem('name');
+const age1 = localStorage.getItem('age');
 
 // clear local storage
 localStorage.clear();
-console.log(name, age);
+console.log(name1, age1);
 
 document.querySelector('form').addEventListener('submit', function(e) {
     e.preventDefault();
@@ -376,43 +424,100 @@ document.querySelector('form').addEventListener('submit', function(e) {
 
 const tasks = JSON.parse(localStorage.getItem('tasks'));
 tasks.forEach(function(task) {
-    console
-    console
-    console
-    console
-    console
-    console
-    console
-    console
-    console
-
     console.log(task);
-}
+});
 
-// JSON
-
-const todos = [
-    {
-        id: 1,
-        text: 'Take out trash',
-        isCompleted: true
-    },
-    {
-        id: 2,
-        text: 'Meeting with boss',
-        isCompleted: true
-    },
-    {
-        id: 3,
-        text: 'Dentist appt',
-        isCompleted: false
-    }
-];
-
-console.log(JSON.stringify(todos));
-
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// // ELEMENT SELECTORS
+//
+// // Single Element Selectors
+// console.log(document.getElementById('my-form'));
+// console.log(document.querySelector('.container'));
+// console.log(document.querySelector('h1'));
+//
+// // Multiple Element Selectors
+// console.log(document.querySelectorAll('.item'));
+// console.log(document.getElementsByTagName('li'));
+// console.log(document.getElementsByClassName('item'));
+//
+// const items = document.querySelectorAll('.item');
+// items.forEach((item) => console.log(item));
+//
+//
+// // MANIPULATING THE DOM
+// const ul = document.querySelector('.items');
+// // ul.remove();
+// // ul.lastElementChild.remove();
+// ul.firstElementChild.textContent = 'Hello';
+// ul.children[1].innerText = 'Brad';
+// ul.lastElementChild.innerHTML = '<h1>Hello</h1>';
+//
+// const btn = document.querySelector('.btn');
+// // btn.style.background = 'red';
+//
+//
+// // EVENTS
+//
+// // Mouse Event
+// btn.addEventListener('click', e => {
+//   e.preventDefault();
+//   console.log(e.target.className);
+//   document.getElementById('my-form').style.background = '#ccc';
+//   document.querySelector('body').classList.add('bg-dark');
+//   ul.lastElementChild.innerHTML = '<h1>Changed</h1>';
+// });
+//
+// // Keyboard Event
+// const nameInput = document.querySelector('#name');
+// nameInput.addEventListener('input', e => {
+//   document.querySelector('.container').append(nameInput.value);
+// });
+//
+//
+// // USER FORM SCRIPT
+//
+// // Put DOM elements into variables
+// const myForm = document.querySelector('#my-form');
+// const nameInput = document.querySelector('#name');
+// const emailInput = document.querySelector('#email');
+// const msg = document.querySelector('.msg');
+// const userList = document.querySelector('#users');
+//
+// // Listen for form submit
+// myForm.addEventListener('submit', onSubmit);
+//
+// function onSubmit(e) {
+//   e.preventDefault();
+//
+//   if(nameInput.value === '' || emailInput.value === '') {
+//     // alert('Please enter all fields');
+//     msg.classList.add('error');
+//     msg.innerHTML = 'Please enter all fields';
+//
+//     // Remove error after 3 seconds
+//     setTimeout(() => msg.remove(), 3000);
+//   } else {
+//     // Create new list item with user
+//     const li = document.createElement('li');
+//
+//     // Add text node with input values
+//     li.appendChild(document.createTextNode(`${nameInput.value}: ${emailInput.value}`));
+//
+//     // Add HTML
+//     // li.innerHTML = `<strong>${nameInput.value}</strong>e: ${emailInput.value}`;
+//
+//     // Append to ul
+//     userList.appendChild(li);
+//
+//     // Clear fields
+//     nameInput.value = '';
+//     emailInput.value = '';
+//   }
+// }
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Asynchronous JavaScript
 // Synchronous
+// Single Threaded
 // Asynchronous
 // AJAX
 // XHR Object
@@ -428,6 +533,32 @@ console.log(JSON.stringify(todos));
 // Form Validation
 // Local & Session Storage
 // Cookies
+// JavaScript Libraries
+// Frameworks
+// Web Components
+// Browser Support
+// Polyfills
+// Feature Detection
+// Babel
+// TypeScript
+// JavaScript Engines
+// V8
+// SpiderMonkey
+// Chakra
+// JavaScript Runtime
+// Call Stack
+// Memory Heap
+// Stack Overflow
+// Memory Leaks
+// Garbage Collection
+// Event Loop
+// Callback Queue
+// Microtask Queue
+// Job Queue
+// Task Queue
+// Web APIs
+
+
 
 
 
