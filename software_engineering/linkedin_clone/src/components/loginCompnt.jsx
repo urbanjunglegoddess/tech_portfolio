d86e2cda4e8491063e0882c1ce58f1e15a91cb2c
Omegea Hunter <omegeadhunter@gmail.com>
2024-09-18 07:18:30 -0400

    Changing location

--- software_engineering/linkedin_clone/src/components/loginCompnt.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export default function LoginCompnt() {
-    return (
-        <div><h1>LoginCompnt</h1></div>
-    )
-}
--- software_engineering/linkedin_clone/src/pages/login.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-import LoginCompnt from "../components/loginCompnt.jsx";
-export default function Login() {
-    return <LoginCompnt />
-}
