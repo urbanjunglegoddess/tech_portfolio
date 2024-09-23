e89c350c332ffee4f832b9f30ae55a9f3dc1839e
Omegea Hunter <omegeadhunter@gmail.com>
2024-09-23 12:12:59 -0400

    Updated Blog Site Location

--- blog_site/blog-site.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('prevBtn').addEventListener('click', function() {
-    document.getElementById('carousel').scrollBy(-300, 0);
-});
-
-document.getElementById('nextBtn').addEventListener('click', function() {
-    document.getElementById('carousel').scrollBy(300, 0);
-});
-
